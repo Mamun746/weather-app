4a59e57cceb0d3dbb48e126d4dbe408de7f8ce4c
Mamun <abdullahalmamun914@gmail.com>
2019-04-02 12:14:48 +0600

    install request npm package

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('hi');
+const request=require('request')
+
+const url='https://api.darksky.net/forecast/f54578bb850cd16dba340c5d995c863c/37.8267,-122.4233'
+
+// request(url, function (error, response, body) {
+//   console.log('error:', error); 
+//   console.log('statusCode:', response && response.statusCode); 
+//   console.log('body:', response); 
+// });
+
+request({url},(err,res)=>{
+    console.log(res);
+    
+
+})
